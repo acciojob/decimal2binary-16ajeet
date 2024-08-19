0569d9be4062b2915cd7462d4a54e367839fc2e2
Ajeet Kumar <98801990+16ajeet@users.noreply.github.com>
2024-08-19 12:11:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function decimalToBinary(num) {
   //Write you code here
+	const binDig = [];
+	do {
+		binDig.push(num%2);
+		console.log(num)
+		num = Math.floor(num/2);
+		console.log(num)
+		if(num < 2) binDig.push(num)
+	} while (num >= 2)
+
+	console.log((binDig.join('')))
   
 }
 
